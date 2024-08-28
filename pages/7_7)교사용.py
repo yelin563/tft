1e29ec0eeea7cc604f7060073c1274dceb1bceb9
yelin563 <126746403+yelin563@users.noreply.github.com>
2024-08-28 11:27:13 +0900

    Update 7_7)교사용.py

--- pages/7_7)교사용.py
+++ pages/7_7)교사용.py
@@ -21,7 +21,7 @@ st.header('수락중 2학년 전략들')
 if st.button('새로고침'):
     
     conn = st.connection("gsheets", type=GSheetsConnection)
-    rdf = conn.read(spreadsheet="pets",
+    rdf = conn.read(
     worksheet="response1",
     ttl="1s",
     usecols=[0,1,2,3],
