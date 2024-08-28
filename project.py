7aed22a8354cd2b4a8a630c84759b88f51095d21
yelin563 <126746403+yelin563@users.noreply.github.com>
2024-08-28 11:14:58 +0900

    Update project.py

--- project.py
+++ project.py
@@ -177,8 +177,9 @@ def tournament(lst1,lst2):
 def name():
     
     num = st.text_input("예:20123홍길동")
+    newnum = re.sub(r'\s+', '', num)
     if st.button("제출하기"):
-        st.session_state.name = num
+        st.session_state.name = newnum
         st.rerun()
 
 if "name" not in st.session_state:
