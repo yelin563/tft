6ad915a523b434e6a1337c4801a94489f856d824
yelin563 <126746403+yelin563@users.noreply.github.com>
2024-09-04 12:45:00 +0900

    Update project.py

--- project.py
+++ project.py
@@ -315,12 +315,12 @@ if b1:
         
         # Add a new column to check if the strategy is in the top 10% for the current round
         current_results['Top 10% 여부'] = current_results['총점'] >= top_10_cutoff_current
-        with col1:
+        with col2:
             # Display the current results with the top 10% check
             st.write("### 현재 라운드 결과 (상위 10% 체크)")
             st.dataframe(current_results.sort_values('총점', ascending=False, ignore_index=True), width=500, height=400)
         
-        with col2:
+        with col3:
             st.write("### 각 전략이 상위 10%에 속할 확률")
             st.dataframe(prob_df.sort_values('Top 10% 확률', ascending=False, ignore_index=True), width=500, height=400)
     else:
